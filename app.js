67a1bc2cc501b2548b23903716743c29b86faa75
KrishaPandey <krishapandey2001@gmail.com>
2024-04-18 21:41:38 +0545

    learned about ejs and nodemon

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const express=require('express')
+const express= require('express')
 const app=express()
+
+app.set("view engine","ejs") //view engine of our project is set to ejs so our ui will be rendered through ejs
 app.get("/",(req,res)=>{
-    res.send("Hello World")
+    const data={name:"Kusma",age:22}
+    res.render("home",{data})
 })
-app.listen(1000,()=>{
-    console.log(" server has started on port 1000 ")
+app.get("/blog/create",(req,res)=>{
+    res.render("blog")
+}) 
+app.get("/blog/edit",(req,res)=>{
+    res.render("edit")
 })
-))))
+app.get("/blog",(req,res)=>{
+    res.send("Single blog")
+})
+app.listen(3000,()=>{
+    console.log(" server has started on port 3000 ")
+}) 
+  
