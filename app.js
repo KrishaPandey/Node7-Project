4763a655a0bbef97e8ce4edc49743c9d95169302
KrishaPandey <krishapandey2001@gmail.com>
2024-04-23 22:12:29 +0545

    create function done

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const express= require('express')
+const express= require('express');
+const { blogs } = require('./model/index');
 const app=express()
 
-require("./model/index");
+require("./model/index"); 
 app.set("view engine","ejs") //view engine of our project is set to ejs so our ui will be rendered through ejs
+app.use(express.json())// postman or react js bata pathayo bhane yo chahincha 
+app.use(express.urlencoded({extended:true}))//incoming data bujh bhaneko ho 
 app.get("/",(req,res)=>{
     res.render("home")
 })
@@ -15,6 +18,21 @@ app.get("/blog/edit",(req,res)=>{
 app.get("/blog",(req,res)=>{
     res.render("blog")
 })
+
+app.post("/blog", async (req,res)=>{
+    // const title=req.body.title
+    // const subtitle=req.body.subtitle
+    // const description=req.body.description
+
+    const{title,subtitle,description}=req.body
+    await blogs.create({
+        title:title,
+        subTitle:subtitle,
+        description:description
+    })
+    res.redirect("/")
+})
+
 app.listen(3000,()=>{
     console.log(" server has started on port 3000 ")
 }) 
